d00ae5bcc89a9230bd45bfc624ed3533160237d1
madhav23bansal <madhavb230100@gmail.com>
2022-11-30 13:20:22 +0530

    initial commit

---
+++ packages/ui/src/button/index.web.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  GestureResponderEvent,
+  Text,
+} from "react-native";
+
+export interface ButtonProps {
+  onClick?: (event: GestureResponderEvent) => void;
+}
+
+export function Button({ onClick }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onClick}>
+      <Text style={styles.text}>Web Button</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    maxWidth: 200,
+    textAlign: "center",
+    borderRadius: 10,
+    paddingTop: 14,
+    paddingBottom: 14,
+    paddingLeft: 30,
+    paddingRight: 30,
+    fontSize: "15px",
+    backgroundColor: "#2f80ed",
+  },
+  text: {
+    color: "white",
+  },
+});
